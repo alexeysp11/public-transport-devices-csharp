0aba4286d7729c037e2ee2e970a8768944ed0064
alexeysp11 <alexeyspiridon1111@gmail.com>
2023-07-23 16:59:02 +0500

    edited rabbitmq examples

--- examples/RabbitMQSender/Program.cs
+++ examples/RabbitMQSender/Program.cs
@@ -8,49 +8,36 @@ using PublicTransportDevices.Models;
 var factory = new ConnectionFactory { HostName = "localhost" };
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
-
 channel.ExchangeDeclare(exchange: "test_exchange", type: ExchangeType.Direct);
-
-var dt1 = System.DateTime.Now; 
 
 // Send asynchronously 
 for (int i = 0; i < 100; i++)
 {
     Thread thread = new Thread(() => 
     {
-        string message = "Hello World! thread: " + Thread.CurrentThread.ManagedThreadId; 
-        var body = Encoding.UTF8.GetBytes(message);
-        channel.BasicPublish(exchange: "test_exchange",
-                            routingKey: "",
-                            basicProperties: null,
-                            body: body);
-        Thread.Sleep(5); 
-        if (j == 99)
-            Console.WriteLine($"Thread '{Thread.CurrentThread.ManagedThreadId}' finished"); 
+        var maxj = 100; 
+        var dt1 = System.DateTime.Now; 
+        for (int j = 0; j < maxj; j++)
+        {
+            string message = "Hello World! thread: " + Thread.CurrentThread.ManagedThreadId; 
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "test_exchange",
+                                routingKey: "",
+                                basicProperties: null,
+                                body: body);
+            Thread.Sleep(5); 
+            if (j == maxj - 1)
+            {
+                var dt2 = System.DateTime.Now; 
+                var dif = dt2 - dt1; 
+                Console.WriteLine($"Thread '{Thread.CurrentThread.ManagedThreadId}' finished (Started: {dt1}, Executed: {dt2}, Executed in: {dif.Seconds}:{dif.Milliseconds})"); 
+            }
+        }
     }); 
     thread.Start(); 
 }
-
-// Send synchronously 
-// string message = "Hello World! ";
-// var body = Encoding.UTF8.GetBytes(message);
-// channel.BasicPublish(exchange: string.Empty,
-//                      routingKey: "hello",
-//                      basicProperties: null,
-//                      body: body);
-// Console.WriteLine($" [x] Sent {message}");
-
-// 
-var dt2 = System.DateTime.Now; 
-var dif = dt2 - dt1; 
-Console.WriteLine($"Started: {dt1}");
-Console.WriteLine($"Executed: {dt2}");
-Console.WriteLine($"Executed in: {dif.Seconds}:{dif.Milliseconds}");
 
 // 
 Console.ReadLine();
 channel.Close(); 
 connection.Close(); 
-
-// Console.WriteLine(" Press [enter] to exit.");
-// Console.ReadLine();
